cee95c978128c5d3a1a78933d85d0e649901caeb
Arsenii <arsenii@gordspace.ru>
2023-06-19 13:59:31 +0000

    Project imports

---
+++ imports.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import zipfile
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import pickle
+
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import ToTensor, CenterCrop
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+torch.manual_seed(4)
